dcfd9a0465aef1a742c3b25553d8654aff090696
Danny <dannypen@gmail.com>
2020-03-02 17:21:24 +0000

    Added if statement to make sure inputs are valid, preventing this kind of error

--- src/main/scala/day1/strings/Main.scala
+++ src/main/scala/day1/strings/Main.scala
@@ -3,9 +3,13 @@ package day1.strings
 object Main extends App {
 
   def cut(word: String, num: Int) = {
-    var num1 = word.length() - num
-    word.substring(num1)
-
+    if (num >word.length() | num<0) {
+      "Number within the length of the word!"
+    }
+    else {
+      var num1 = word.length() - num
+      word.substring(num1)
+    }
   }
   println(cut("Hello", 2))
 }
