a35403a88d4a79a4bedb00ff31b48c3a4b9c5a5c
Danny <dannypen@gmail.com>
2020-03-03 11:49:11 +0000

    Added Iteration 3 solution

--- src/main/scala/day2/iteration/Main.scala
+++ src/main/scala/day2/iteration/Main.scala
@@ -1,5 +1,7 @@
 package day2.iteration
 
+import scala.collection.mutable.ListBuffer
+
 object Main extends App {
 
   def wordMultiply(word: String, num: Int): Unit = {
@@ -14,5 +16,25 @@ object Main extends App {
       println(word*num)
     }
   }
-  rectangle("z",5)
+  // rectangle("z",5)
+
+  def fizzBuzz(fizz: String, buzz: String, num: Int):String = {
+    val fin = ListBuffer.empty[String]
+    for (i <- 1 to num) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        fin += (fizz + buzz)
+      }
+      else if (i % 3 == 0) {
+        fin += (buzz)
+      }
+      else if (i % 5 == 0) {
+        fin += (fizz)
+      }
+      else {
+        fin += (i.toString)
+      }
+    }
+    fin.toString
+  }
+  println(fizzBuzz("Fizz","Buzz",15))
 }
