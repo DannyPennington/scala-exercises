7acff00cc5cf1552603ff9e4862c17fd953b0a3b
Danny <dannypen@gmail.com>
2020-03-05 12:48:45 +0000

    Implemented shoot function and changed board to be "~" instead of "0"

---
+++ src/main/scala/day4/battleships/Board.scala
@@ -0,0 +1,63 @@
+package day4.battleships
+
+class Board() {
+  var arr: Array[Array[String]] = Array.ofDim[String](12, 12)
+  for (i <- 0 until 12) {
+    for (j <- 0 until 12) {
+      arr(i)(j) = " ~ "
+    }
+  }
+
+  def printGrid(grid: Array[Array[String]]= arr, size: Int = 12): Unit = {
+    print("      A  B  C  D  E  F  G  H  I  J  K  L")
+    println("")
+    for (i <- 0 until size) {
+      if (i < 9) {
+        print(s"  ${i + 1}  ")
+      }
+      else {
+        print(s"  ${i + 1} ")
+      }
+      grid(i).foreach(print)
+      println("")
+    }
+    println("")
+  }
+
+  def addShip(board: Array[Array[String]], length: Int, y: Int, x: Int, direction: String): Array[Array[String]] = {
+    board(y - 1)(x - 1) = " 1 "
+    direction match {
+      case "up" | "Up" => {for (i <- 1 to length) {board(y - i)(x - 1) = " 1 "}}
+      case "down" | "Down" => {for (i <- 1 to length) {board(y + i - 2)(x - 1) = " 1 "}}
+      case "left" | "Left" => {for (i <- 1 to length) {board(y - 1)(x - i) = " 1 "}}
+      case "right" | "Right" => {for (i <- 1 to length) {board(y - 1)(x + i - 2) = " 1 "}}
+    }
+    board
+  }
+
+  def shoot(board: Array[Array[String]] = arr,y:Int, x:Int ):Boolean = {
+    if (board(y-1)(x-1) == " 1 ") {
+      println("That's a hit")
+      board(y-1)(x-1) = " X "
+      true
+    }
+    else{ println("You missed")
+      board(y-1)(x-1) = " O "
+      false
+    }
+
+  }
+
+  def lose(board: Array[Array[String]] = arr):Boolean = {
+    for (i <- 0 until 12) {
+      for (j <- 0 until 12) {
+        println(board(i)(j))
+        if (board(i)(j) == " 1 ") {
+          false
+        }
+        else {}
+      }
+    }
+    true
+  }
+}
--- src/main/scala/day4/battleships/Main.scala
+++ src/main/scala/day4/battleships/Main.scala
@@ -6,39 +6,13 @@ import scala.collection.mutable.ArrayBuffer
 
 object Main extends App {
 
-  def createGrid(size:Int): Array[Array[String]] = {
-    var arr = Array.ofDim[String](size,size)
-    for (i <- 0 until size) {
-      for (j <- 0 until size) {
-        arr(i)(j) = " 0 "
-      }
-    }
-    arr
-  }
 
-  def printGrid(grid:Array[Array[String]], size:Int=12):Unit = {
-    print("      A  B  C  D  E  F  G  H  I  J  K  L")
-    println("")
-    for (i <- 0 until size) {
-      if (i < 9) {print(s"  ${i+1}  ")}
-      else {print(s"  ${i+1} ")}
-      grid(i).foreach(print)
-      println("")
-    }
-  }
-
-  def addShip(board: Array[Array[String]],length:Int, y:Int, x:Int, direction: String): Array[Array[String]]= {
-    board(y-1)(x-1) = " 1 "
-    direction match {
-      case "up" | "Up" => {for (i <- 1 to length) {board(y-i)(x-1) = " 1 "}}
-      case "down" | "Down" => {for (i <- 1 to length) {board(y+i-2)(x-1) = " 1 "}}
-      case "left" | "Left" => {for (i <- 1 to length) {board(y-1)(x-i) = " 1 "}}
-      case "right" | "Right" => {for (i <- 1 to length) {board(y-1)(x+i-2) = " 1 "}}
-    }
-    board
-  }
-
-  var board = createGrid(12)
-  printGrid(addShip(board,3,2,10,"right"))
+  val player1 = new Board
+  val player2 = new Board
+  player1.addShip(player1.arr,3,2,5,"right")
+  player1.shoot(y=2,x=5)
+  player1.shoot(y=5,x=5)
+  player1.printGrid()
+  println(player1.lose())
 
 }
