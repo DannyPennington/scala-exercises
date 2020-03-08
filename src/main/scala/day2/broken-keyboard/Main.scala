a2f6f92d8e5064a7673aa8a30defd95f3928e728
Danny <dannypen@gmail.com>
2020-03-08 17:16:40 +0000

    Improved to use pattern matching

--- src/main/scala/day2/broken-keyboard/Main.scala
+++ src/main/scala/day2/broken-keyboard/Main.scala
@@ -4,22 +4,20 @@ object Main extends App {
   def letterFind(letters: String): Unit= {
     val lines = Source.fromFile("/home/qa-admin/Downloads/dictionary.txt").getLines.toArray
     val words = scala.collection.mutable.ArrayBuffer.empty[String]
-    val let = letters.toList
     for (word <- lines) {
-      var x = word.length()
-      for (i <- (0 until word.length())) {
-        if (!let.contains(word(i))) {
-          x -= 1
+      var x = 0
+      for (i <- (0 until word.length)) {
+        if (!letters.toList.contains(word(i))) {
+          x += 1
         }
       }
-      if (x == word.length()) {
-        words += word
+      x match {
+        case 0 => words += word
+        case _ =>
       }
-      }
-    //println(words)
+    }
     println(words.maxBy(_.length))
     }
-
   // Asking for user input
   val number = scala.io.StdIn.readLine("How many keyboards? ").toInt
   for (i <- (0 until number)) {
--- src/main/scala/day2/conditionals/Main.scala
+++ src/main/scala/day2/conditionals/Main.scala
@@ -3,11 +3,10 @@ package day2.conditionals
 object Main extends App {
 
   def sum2(num1: Int,num2: Int, con: Boolean): Int = {
-    if (con == true) {
-      num1+num2
-    }
-    else {
-      num1*num2
+    con match {
+      case true => num1+num2
+      case false => num1*num2
+      case _ => println("How did this even happen?"); 0
     }
   }
   def conditional(num1: Int,num2: Int, con: Boolean): Int = {
@@ -24,5 +23,6 @@ object Main extends App {
       num1*num2
     }
   }
-  println(conditional(4,0,false))
+  //println(conditional(4,0,false))
+  println(sum2(5,5,false))
 }
